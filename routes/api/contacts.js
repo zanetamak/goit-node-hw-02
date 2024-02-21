ee19e884edb7f6d7df9883db27e13005ee8dfc91
Żaneta Makowska <129063903+zanetamak@users.noreply.github.com>
2024-02-21 21:29:44 +0100

    Update routes/api/contacts.js
    Co-authored-by: Kamil Mrowka <68347303+KamilMr@users.noreply.github.com>

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -27,15 +27,14 @@ router.get('/:contactId', async (req, res, next) => {
   try {
     const { contactId } = req.params;
     const contact = await getContactById(contactId);
-    if (contact) {
+    if (contact) 
       return res
         .status(200)
         .json({ status: 'success', code: 200, data: { contact } });
-    } else {
+
       return res
         .status(404)
         .json({ status: 'error', code: 404, message: 'Not found' });
-    }
   } catch (error) {
     next(error);
   }
